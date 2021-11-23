cbdcf112eb2b194eb34e44d1179990de02947b97
Aidansg00 <asgennuso@txwes.edu>
2021-11-23 17:21:20 -0600

    Enemy 1 updates (not complete)

--- app/src/main/java/com/example/gameproject/EnemyAI/LevelOne.java
+++ app/src/main/java/com/example/gameproject/EnemyAI/LevelOne.java
@@ -2,18 +2,72 @@ package com.example.gameproject.EnemyAI;
 
 import android.content.Context;
 
-import androidx.core.content.ContextCompat;
-
 import com.example.gameproject.GameObjects.Enemy;
 import com.example.gameproject.GameObjects.Player;
-import com.example.gameproject.Infrastructure.GameLoop;
 
 public class LevelOne extends Enemy{
-    public static final double SPEED_PIXELS_PER_SECOND = 200.0;
-    private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
-    public static final int MAX_HEALTH_POINTS = 1;
     public LevelOne(Context context, Player player, double positionX, double positionY, double radius) {
         super(context, player, positionX, positionY, radius);
-
     }
+
+    int maxSpeed = (int) (MAX_SPEED*0.5);
+    int moveSpeed = (int) (SPEED_PIXELS_PER_SECOND*0.5);
+    int fallSpeed = maxSpeed;
+    boolean facingRight = true;
+
+    private void getNextPosition(){
+
+        //movement of enemy 1
+        if(left){
+            dx -= moveSpeed;
+            if(dx < -maxSpeed){
+                dx = -maxSpeed;
+            }
+        }
+        else if(right){
+            dx += moveSpeed;
+            if(dx > maxSpeed){
+                dx = maxSpeed;
+            }
+        }
+
+        if(falling){
+            dy += fallSpeed;
+        }
+    }
+
+    public void updateEnemy1(){
+        getNextPosition();
+        checkTileMapCollision();
+        setPosition(xtemp,ytemp);
+
+        //switch direction if collision occurs
+        if(right && dx == 0){
+            right = false;
+            left = true;
+            facingRight = false;
+        }
+        else if(left && dx == 0){
+            right = true;
+            left = false;
+            facingRight = true;
+        }
+    }
+
+    //animation flip
+    public void draw(){
+
+        if(notOnScreen()) return;
+
+        setMapPosition();
+
+        if(facingRight){
+            g.drawImage(animation.getImage(), (int) (x+xmap-width/2), (int) (y+ymap-height/2),null);
+        }
+        else{
+            g.drawImage(animation.getImage(), (int) (x+xmap-width/2+width), (int) (y+ymap-height/2), -width, height, null);
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/gameproject/GameObjects/Enemy.java
+++ app/src/main/java/com/example/gameproject/GameObjects/Enemy.java
@@ -9,13 +9,14 @@ import com.example.gameproject.R;
 
 public class Enemy extends Circle
 {
-    private static final double SPEED_PIXELS_PER_SECOND = Player.SPEED_PIXELS_PER_SECOND*0.6;
-    private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
-    private static final double SPAWNS_PER_MINUTE = 20;
-    private static final double SPAWNS_PER_SECOND = SPAWNS_PER_MINUTE/60.0;
-    private static final double UPDATES_PER_SPAWN = GameLoop.MAX_UPS/SPAWNS_PER_SECOND;
-    private static double updatesUntilNextSpawn = UPDATES_PER_SPAWN;
-    private Player player;
+    protected double SPEED_PIXELS_PER_SECOND = Player.SPEED_PIXELS_PER_SECOND;
+    protected double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
+    protected int MAX_HEALTH_POINTS = 1;
+    protected double SPAWNS_PER_MINUTE = 20;
+    protected double SPAWNS_PER_SECOND = SPAWNS_PER_MINUTE/60.0;
+    protected double UPDATES_PER_SPAWN = GameLoop.MAX_UPS/SPAWNS_PER_SECOND;
+    protected double updatesUntilNextSpawn = UPDATES_PER_SPAWN;
+    protected Player player;
 
     public Enemy(Context context, Player player, double positionX, double positionY, double radius)
     {
