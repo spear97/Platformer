2d8a1d70e72eb7928b989a52d3a3caec3c566f17
koantonio <kara.antonio19@gmail.com>
2021-11-28 15:22:21 -0600

    Menu Updates

--- app/src/main/java/com/example/gameproject/Infrastructure/MainActivity.java
+++ app/src/main/java/com/example/gameproject/Infrastructure/MainActivity.java
@@ -1,12 +1,29 @@
 package com.example.gameproject.Infrastructure;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gameproject.R;
+
 
 public class MainActivity extends AppCompatActivity
 {
+    //upon button click
+    public void OnTap(View view) {
+        setContentView(new Game(this));
+    }
+
+    private enum STATE{
+        MENU,
+        GAME
+    };
+
+    //use STATE variable to check what State we are in
+    private STATE State=STATE.MENU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,7 +34,12 @@ public class MainActivity extends AppCompatActivity
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //Set ContentView to Game, so that objects in the Game Class can be rendered to the Screen
-        setContentView(new Game(this));
+        if(State==STATE.GAME) {
+            //Set ContentView to Game, so that objects in the Game Class can be rendered to the Screen
+            setContentView(new Game(this));
+        }
+        else if(State==STATE.MENU){
+            setContentView(R.layout.activity_main);
+        }
     }
 }
