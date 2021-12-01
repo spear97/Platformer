4e497e54efdbd67d631bda2543deb00481431049
spear97 <72108425+spear97@users.noreply.github.com>
2021-12-01 12:58:06 -0600

    Update

--- app/src/main/java/com/example/gameproject/Infrastructure/MainActivity.java
+++ app/src/main/java/com/example/gameproject/Infrastructure/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.gameproject.Infrastructure;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-
+import android.media.MediaPlayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gameproject.R;
@@ -12,15 +10,21 @@ import com.example.gameproject.R;
 
 public class MainActivity extends AppCompatActivity
 {
+    MediaPlayer bkgrdmsc;
     //upon button click
     public void OnTap(View view) {
         setContentView(new Game(this));
+        bkgrdmsc=MediaPlayer.create(MainActivity.this, R.raw.bakground);
+        bkgrdmsc.setLooping(true);
+        bkgrdmsc.start();
     }
+    public void OnTap2(View view) {setContentView(R.layout.layout);}
+    public void OnTap3(View view) {setContentView(R.layout.activity_main);}
 
     private enum STATE{
         MENU,
         GAME
-    };
+    }
 
     //use STATE variable to check what State we are in
     private STATE State=STATE.MENU;
@@ -29,10 +33,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         //Set window to fullscreen (will hide status bar)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
 
         if(State==STATE.GAME) {
             //Set ContentView to Game, so that objects in the Game Class can be rendered to the Screen
@@ -41,5 +46,11 @@ public class MainActivity extends AppCompatActivity
         else if(State==STATE.MENU){
             setContentView(R.layout.activity_main);
         }
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        bkgrdmsc.release();
+        finish();
     }
 }
